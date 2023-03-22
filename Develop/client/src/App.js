7de0c30797e3d1d320e4bd97a80b36380dcdea60
JoeBosz <joeboz2213@gmail.com>
2023-03-22 12:42:50 -0500

    confused

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+//import SearchBooks from './pages/SearchBooks';
+//import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+  }
+  }
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
         <Routes>
-          <Route 
+          {/* <Route 
             path='/' 
             element={<SearchBooks />} 
           />
@@ -21,10 +44,11 @@ function App() {
           <Route 
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
-          />
+          /> */}
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
